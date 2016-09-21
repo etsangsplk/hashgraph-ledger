ef446c9951a5d8155d59962e2f32562ad26309dd
Stefan Buhrmester <buhrmi@gmail.com>
2016-09-21 13:54:12 +0900

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,7 +1,6 @@
-// This file contains some simple general-purpose contracts that can be deployed to the blockchain.
-// a contract is simply a promise body that "promises" to modify
-// the ledgerState in a regulated and truthful way.
-// The contract MUST NOT use random values
+// This file contains some simple general-purpose contracts that can be deployed to the ledger.
+// a contract is simply a promise body that "promises" to modify the ledger in a way
+// A contract has to be "pure", meaning that given a ledger, it has to modify the ledger in the same way regardless of time or location of execution 
 
 module.exports = {
   // Upon deployment, this contract simply stores a value under a specified identifier.
